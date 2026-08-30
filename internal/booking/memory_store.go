809a82eee3d87b2de6a06d59653d9482db215c0f
Abuhurrara <abuhurrara1093@gmail.com>
2026-08-31 04:45:17 +0500

    Implement list booking endpoint

--- internal/booking/memory_store.go
+++ internal/booking/memory_store.go
@@ -15,8 +15,16 @@ func (s *MemoryStore) Book(b Booking) (Booking, error) {
 	if _, exists := s.bookings[b.SeatID]; exists {
 		return b, ErrSeatAlreadyExists
 	}
+	s.bookings[b.SeatID] = b
+	return b, nil
 }
 
 func (s *MemoryStore) ListBookings(movieID string) []Booking {
-
+	var result []Booking
+	for _, b := range s.bookings {
+		if b.MovieID == movieID {
+			result = append(result, b)
+		}
+	}
+	return result
 }
